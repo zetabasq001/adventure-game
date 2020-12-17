5105e992c84d1e193c343265a03cc31b80152918
George Zaldivar <gzald001@gmail.com>
2020-12-17 16:56:37 -0500

    feat: define function enter_query

--- adventure_game.py
+++ adventure_game.py
@@ -13,9 +13,26 @@ def intro():
     messages = ["You find yourself standing in an open field, ",
     "filled with grass and yellow wildflowers. ", 
     f"Rumor has it that a {villain} is somewhere around here, ",
-    "and has been terrifying the nearby village."]
+    "and has been terrifying the nearby village.", 
+    "In front of you is a house.",
+    "To your right is a dark cave.",
+    "In your hand you hold your trusty (but not very effective) dagger."]
 
     for message in messages:
         print_pause(message)
 
-intro()
+
+def enter_query():
+    queries = ["Enter 1 to knock on the door of the house.",
+    "Enter 2 to peer into the cave.",
+    "What would you like to do?",
+    "(Please enter 1 or 2.)"]
+
+    number = ''
+    for query in queries:
+        print_pause(query)
+    number = input()
+
+
+
+
