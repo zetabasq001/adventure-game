57a273c8b17f15bbd01baf6527cc244fcaab3665
George Zaldivar <gzald001@gmail.com>
2020-12-17 16:30:36 -0500

    feat: define function intro

--- adventure_game.py
+++ adventure_game.py
@@ -6,3 +6,16 @@ def print_pause(message):
     time.sleep(2)
 
 
+def intro():
+    villains = ["deadly dragon", "wicked witch", "sinister sorcerer"]
+    villain = random.choice(villains)
+
+    messages = ["You find yourself standing in an open field, ",
+    "filled with grass and yellow wildflowers. ", 
+    f"Rumor has it that a {villain} is somewhere around here, ",
+    "and has been terrifying the nearby village."]
+
+    for message in messages:
+        print_pause(message)
+
+intro()
