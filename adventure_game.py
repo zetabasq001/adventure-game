ab75fbb2b02ed3ce579a9dda69d18500ab090e1a
George Zaldivar <gzald001@gmail.com>
2020-12-18 18:28:29 -0500

    style: conform to pycodestyle

--- adventure_game.py
+++ adventure_game.py
@@ -1,29 +1,29 @@
 import time
 import random
 
-def print_pause(messages):
 
+def print_pause(messages):
     if len(messages) == 0:
         return
     else:
         print(messages[0])
-        #time.sleep(1)
+        time.sleep(2)
     print_pause(messages[1:])
 
 
 def start():
-
     villains = ["deadly dragon", "wicked witch", "sinister sorcerer",
-        "gigantic gorgon", "treacherous troll", "weird wizard"]
+                "gigantic gorgon", "treacherous troll", "weird wizard"]
     villain = random.choice(villains)
 
-    messages = ["You find yourself standing in an open field, ",
-        "filled with grass and yellow wildflowers. ", 
-        f"Rumor has it that a {villain} is somewhere around here, ",
-        "and has been terrifying the nearby village.", 
-        "In front of you is a house.",
-        "To your right is a dark cave.",
-        "In your hand you hold your trusty (but not very effective) dagger."]
+    messages = ["You find yourself standing in an open field,",
+                "filled with grass and yellow wildflowers.",
+                f"Rumor has it that a {villain} is somewhere around here,",
+                "and has been terrifying the nearby village.",
+                "In front of you is a house.",
+                "To your right is a dark cave.",
+                "In your hand you hold your trusty",
+                "(but not very effective) dagger."]
 
     print_pause(messages)
     sword = False
@@ -41,17 +41,16 @@ def ending():
 
 
 def house(sword, villain):
-
     messages = ["You approach the door of the house.",
-        "You are about to knock when the door opens",
-        f"and out steps a {villain}.",
-        f"Eep! This is the {villain}'s house!",
-        f"The {villain} attacks you!"]
+                "You are about to knock when the door opens",
+                f"and out steps a {villain}.",
+                f"Eep! This is the {villain}'s house!",
+                f"The {villain} attacks you!"]
     print_pause(messages)
 
     if not sword:
         messages = ["You feel a bit under-prepared for this,",
-                "what with only having a tiny dagger."]
+                    "what with only having a tiny dagger."]
         print_pause(messages)
 
     print_pause(["Would you like to (1) fight or (2) run away?"])
@@ -59,21 +58,20 @@ def house(sword, villain):
 
 
 def cave(sword, villain):
-
     if sword:
         messages = ["You peer cautiously into the cave.",
                     "You've been here before,",
                     "and gotten all the good stuff.",
                     "It's just an empty cave now.",
-                    "You walk back out to the field."]    
+                    "You walk back out to the field."]
     else:
         messages = ["You peer cautiously into the cave.",
-            "It turns out to be only a very small cave.",
-            "Your eye catches a glint of metal behind a rock.",
-            "You have found the magical Sword of Ogoroth!",
-            "You discard your silly old dagger ",
-            "and take the sword with you.",
-            "You walk back out to the field."]
+                    "It turns out to be only a very small cave.",
+                    "Your eye catches a glint of metal behind a rock.",
+                    "You have found the magical Sword of Ogoroth!",
+                    "You discard your silly old dagger ",
+                    "and take the sword with you.",
+                    "You walk back out to the field."]
 
         sword = True
     print_pause(messages)
@@ -81,35 +79,34 @@ def cave(sword, villain):
 
 
 def field(sword, villain):
-
     messages = ["You run back into the field.",
-        "Luckily, you don't seem to have been followed."]
+                "Luckily, you don't seem to have been followed."]
 
     print_pause(messages)
     enter_query(sword, villain)
 
 
 def fight(sword, villain):
-
     if not sword:
         messages = ["You do your best...",
-            f"but your dagger is no match for the {villain}.",
-            "You have been defeated!"]
-        print_pause(messages)   
+                    f"but your dagger is no match for the {villain}.",
+                    "You have been defeated!"]
+        print_pause(messages)
     else:
-        messages = [f"As the {villain} moves to attack, you unsheath your new sword.",
-            "The Sword of Ogoroth shines brightly in your hand",
-            "as you brace yourself for the attack.",
-            f"But the {villain} takes one look at your shiny new toy and runs away!",
-            f"You have rid the town of the {villain}.",
-            "You are victorious!"]
+        messages = [f"As the {villain} moves to attack,",
+                    "you unsheath your new sword.",
+                    "The Sword of Ogoroth shines brightly in your hand",
+                    "as you brace yourself for the attack.",
+                    f"But the {villain} takes one look",
+                    "at your shiny new toy and runs away!",
+                    f"You have rid the town of the {villain}.",
+                    "You are victorious!"]
         print_pause(messages)
 
     ending()
 
 
 def valid_response(number, sword, villain):
-
     if number == '1':
         house(sword, villain)
     elif number == '2':
@@ -125,11 +122,10 @@ def valid_response(number, sword, villain):
 
 
 def enter_query(sword, villain):
-
     queries = ["\nEnter 1 to knock on the door of the house.",
-        "Enter 2 to peer into the cave.",
-        "What would you like to do?",
-        "(Please enter 1 or 2.)"]
+               "Enter 2 to peer into the cave.",
+               "What would you like to do?",
+               "(Please enter 1 or 2.)"]
 
     print_pause(queries)
 
